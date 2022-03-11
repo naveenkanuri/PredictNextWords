a9e628021186ebcedfafb867a8e479f792ce0fb8
naveenkanuri <83157625+naveenkanuri@users.noreply.github.com>
2022-03-10 19:54:20 -0800

    Add files via upload
    Initial Commit

---
+++ kerasServer_60.py
@@ -0,0 +1,95 @@
+import flask
+from flask import request, jsonify
+import model_60 as model
+from flask_cors import CORS
+import requests
+
+storyModel = None
+app = flask.Flask(__name__)
+CORS(app)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = {"success": False}
+    body = request.json
+    test = {}
+    
+    numWords = 10
+    if "inputText" not in body:
+        data["message"] = "Must include inputText in request body"
+        return jsonify(data)
+
+    if "numWords" in body:
+        numWords = body["numWords"]
+
+    inputText = body["inputText"]
+
+    try:
+        pred = model.makePrediction(inputText, storyModel, numWords)
+        data["completedText"] = pred[0]
+        data["addedText"] = pred[1]
+        data["success"] = True
+        if "grammar_check" in body:
+            try:
+                inputText = data["addedText"]
+                test["inputText"] = inputText
+                inputBody = jsonify(test)
+                rr = requests.post("http://localhost:5000/grammar", json=test)
+                rr_json = rr.json()
+                data["newText"] = rr_json["newText"]
+                data["grammar_check"] = "success"
+            except:
+                data["grammar_check"] = "fail"
+    except:
+        data["message"] = "Invalid characters"
+
+    return jsonify(data)
+
+@app.route("/grammar", methods=["POST"])
+def grammar():
+    data = {"success": False}
+    body = request.json
+    
+    if "inputText" not in body:
+        data["message"] = "Must include inputText in request body"
+        return jsonify(data)
+
+    text = body["inputText"]
+    
+    r = requests.post("https://grammarbot.p.rapidapi.com/check",
+        data = {'text': text, 'language': 'en-US'},
+        headers={
+            'x-rapidapi-host': "grammarbot.p.rapidapi.com",
+            'x-rapidapi-key': "",
+            'content-type': "application/x-www-form-urlencoded"
+        })
+
+    j = r.json()
+    print(r)
+    new_text = ''
+    cursor = 0
+    for match in j["matches"]:
+        offset = match["offset"]
+        length = match["length"]
+        if cursor > offset:
+            continue
+        # build new_text from cursor to current offset
+        new_text += text[cursor:offset]
+        # next add first replacement
+        repls = match["replacements"]
+        if repls and len(repls) > 0:
+            new_text += repls[0]["value"]
+        # update cursor
+        cursor = offset + length
+       
+    # if cursor < text length, then add remaining text to new_text
+    if cursor < len(text):
+        new_text += text[cursor:]
+
+    data["newText"] = new_text
+    return jsonify(data)
+
+if __name__ == '__main__':
+    storyModel = model.useModel()
+    model.makePrediction("primer", storyModel, 1)
+    app.run()
---
+++ model_60.py
@@ -0,0 +1,199 @@
+import numpy as np
+import sys
+import nltk
+# nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense, Dropout, LSTM
+from tensorflow.keras.layers import Dense, Activation, LSTM, Dropout, GRU, TimeDistributed, BatchNormalization
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.callbacks import ModelCheckpoint
+import heapq
+import random 
+
+def tokenize(text):
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(text)
+
+    filtered = filter(lambda token: token not in stopwords.words('english'), tokens)
+    # return list(filtered)
+    return " ".join(filtered)
+
+def useModel():
+    text = open("../storyData.txt").read().lower()
+
+    chars = sorted(list(set(text)))
+    numChars = len(chars)
+    seqLen = 60
+
+    model = Sequential()
+    model.add(LSTM(numChars * 5, input_shape=(seqLen, numChars)))
+    model.add(BatchNormalization())
+    model.add(Activation('selu'))
+
+    model.add(Dense(numChars * 2))
+    model.add(BatchNormalization())
+    model.add(Activation('selu'))
+
+    model.add(Dense(numChars * 2))
+    model.add(BatchNormalization())
+    model.add(Activation('selu'))
+
+    model.add(Dense(numChars))
+    model.add(Activation('softmax'))
+
+    optimizer = RMSprop(lr=0.001)
+
+    filename = "model_weights_saved_60.hdf5"
+    model.load_weights(filename)
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+    return model
+
+def sample(preds, top_n=3):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds)
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+
+    return heapq.nlargest(top_n, range(len(preds)), preds.take)
+
+def sampleRandom(preds, top_n=3):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds)
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    sampleList = [100, 200, 300, 400, 500]
+    randomList = random.choices(range(len(preds)), weights=preds) 
+    return heapq.nlargest(top_n, range(len(preds)), preds.take)
+
+def makePrediction(inputSequence, model, numWords):
+    if inputSequence.strip() == "":
+        return (inputSequence,"")
+    # chars = [' ', '!', '"', '#', '$', '&', "'", '(', ')', '*', '+', ',', '-', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '?', '[', ']', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '}', '\xa0', '¢', '£', '¦', '§', '¨', '©', 'ª', '«', '®', '¯', '´', '¶', 'â', 'ã', 'ˆ', '—', '”', '…', '€']
+    text = open("../storyData.txt").read().lower()
+
+    chars = sorted(list(set(text)))
+
+    charToIndices = dict((c, i) for i, c in enumerate(chars))
+    indicesToChar = dict((i, c) for i, c in enumerate(chars))
+    numChars = len(chars)
+    seqLen = 60
+    
+    original_text = inputSequence
+    inputSequence = inputSequence.lower()
+    filtered = ""
+    for c in inputSequence:
+        if c in chars:
+            filtered+=c
+
+    inputSequence = filtered
+
+    if len(inputSequence)>60:
+        inputSequence = inputSequence[-59:]
+
+    while len(inputSequence) <60:
+        inputSequence = " "+inputSequence
+
+    completion = ''
+    gop = True
+    wordCount = 0
+    lastRandom = 0
+
+    while gop:
+        x = np.zeros((1, seqLen, numChars))
+        for t, char in enumerate(inputSequence):
+            x[0, t, charToIndices[char]] = 1.
+        preds = model.predict(x, verbose=0)[0]
+
+        chance = random.randint(1,3)
+        if chance == 1 and inputSequence[len(inputSequence)-1]==" " and lastRandom >5:
+            lastRandom = 0
+            op = sample(preds, top_n=5)
+            rng = random.choices(op, weights=[0,16,8,4,2]) 
+            next_index = rng[0]
+        else:
+            next_index = sample(preds, top_n=1)[0]
+        next_char = indicesToChar[next_index]
+
+        if next_char == ' ':
+            wordCount+=1
+            lastRandom+=1
+
+        inputSequence = inputSequence[1:] + next_char
+        completion += next_char
+        
+        if wordCount >= numWords or len(original_text + completion) > 1500:
+            break
+
+    return (original_text + completion, completion)
+
+def train(): 
+    # text = open("../smallData.txt").read().lower()
+    text = open("../storyData.txt").read().lower()
+
+    lenText = len(text)
+
+    chars = sorted(list(set(text)))
+    charToIndices = dict((c, i) for i, c in enumerate(chars))
+    indicesToChar = dict((i, c) for i, c in enumerate(chars))
+    numChars = len(chars)
+
+    # arbitrary length for a sequence
+    seqLen = 60
+    stepSize = 4
+    inputSeq = []
+    outputChar = []
+    dataPairs = []
+    for i in range(0,lenText-seqLen, stepSize):
+        inputSeq.append(text[i:i+seqLen])
+        outputChar.append(text[i+seqLen])
+        dataPairs.append((text[i:i+seqLen],text[i+seqLen]))
+
+    X = np.zeros((len(inputSeq), seqLen, numChars), dtype=np.bool)
+    y = np.zeros((len(inputSeq), numChars), dtype=np.bool)
+  
+    for i, seq in enumerate(inputSeq):
+        for t, char in enumerate(seq):
+            X[i, t, charToIndices[char]] = True
+        y[i, charToIndices[outputChar[i]]] = True
+    
+    model = Sequential()
+    model.add(LSTM(numChars * 5, input_shape=(seqLen, numChars)))
+    model.add(BatchNormalization())
+    model.add(Activation('selu'))
+
+    model.add(Dense(numChars * 2))
+    model.add(BatchNormalization())
+    model.add(Activation('selu'))
+
+    model.add(Dense(numChars * 2))
+    model.add(BatchNormalization())
+    model.add(Activation('selu'))
+
+    model.add(Dense(numChars))
+    model.add(Activation('softmax'))
+
+    optimizer = RMSprop(lr=0.001)
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+    filepath = "model_weights_saved_60.hdf5"
+    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+    desired_callbacks = [checkpoint]
+
+    model.fit(X, y, validation_split=0.05, batch_size=124, epochs=20, shuffle=True, callbacks=desired_callbacks)
+
+if __name__=="__main__": 
+    model = useModel()
+    while True:
+        choice = input("Enter 'T' to Train. Enter 'P' to make a prediction. Enter anything else to terminate.")
+        if choice.lower() == 't':
+            train()
+        elif choice.lower() == 'p':
+            inputSequence = input("Enter your input sentence/sequence:")
+            makePrediction(inputSequence.lower(), model, 50)
+        else:
+            print("Terminating")
+            break
+    makePrediction()
